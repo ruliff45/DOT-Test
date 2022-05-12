2bcae7a854301b43b149347fb8686554a1bf9788
Edward Ruliff Podianto <ruliff45@gmail.com>
2022-05-12 12:50:37 +0700

    Sprint2 Question1

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\City;
+use App\Models\Province;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class SearchController extends Controller
+{
+    public function swapableImplementation(Request $request)
+    {
+        $result = $request->query();
+        if ($result['category'] === 'city') {
+            if ($result['src'] === 'api') {
+                $api = Http::withHeaders(['key' => '0df6d5bf733214af6c6644eb8717c92c'])->get('https://api.rajaongkir.com/starter/city?id=' . $result['id']);
+                return response()->json($api->body());
+            } else if ($result['src'] === 'db' || $result['src'] === null) {
+                $db = City::find($result['id']);
+                return response()->json($db);
+            }
+        } else if ($result['category'] === 'province') {
+            if ($result['src'] === 'api') {
+                $api = Http::withHeaders(['key' => '0df6d5bf733214af6c6644eb8717c92c'])->get('https://api.rajaongkir.com/starter/province?id=' . $result['id']);
+                return response()->json($api->body());
+            } else if ($result['src'] === 'db' || $result['src'] === null) {
+                $db = Province::where('province_id', $result['id'])->get();
+                return response()->json($db);
+            }
+        }
+    }
+}
